fa329483a8284c782e49c553a7d456e1e741dd12
frozflame <frozflame@outlook.com>
2024-08-09 21:37:27 +0800

    wip 0.0.2 * add changes.txt * export FileField, FileFormKit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./styles/main.scss";
 
-export type {Field} from "./types";
-export {FormKit, ModalKit, FormModalKit, FileFormModalKit, type ModalOptions} from "./kits";
+export type {Field, FileField} from "./types";
+export {FormKit, FileFormKit, ModalKit, FormModalKit, FileFormModalKit, type ModalOptions} from "./kits";
 export {Modal} from "./modal";
 export {Form} from "./form";
 export {FileForm} from "./fileform";
